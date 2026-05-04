b5efaeeec8b8f2ee65e05fae1d029c7f42ac43bb
Cameron <cameronlillis52@gmail.com>
2026-05-03 19:36:22 -0700

    ancitent

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,118 @@
-#include <DallasTemperature.h>
-#include <OneWire.h>
-#include <PID_v1.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
-// #include <SPI.h>
-// #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <RotaryEncoder.h>
+#include <Wire.h>
 
+// --- Pin Definitions ---
+// Change these to match your wiring
 
-// #define SCREEN_WIDTH 132 // OLED display width, in pixels
-// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Rotary Encoder
+constexpr int PIN_ENC_A = 6;
+constexpr int PIN_ENC_B = 5;
+constexpr int PIN_ENC_BTN = 7;
 
-// Pins
-#define VOLTAGE_SENSOR_PIN A0
-#define ONE_WIRE_BUS 2
-#define MOSFET_PIN 3
-#define A A1
-#define B A3
-// OneWire / Temperature setup
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// PID Variables
-double setTemp, tempRead, Output;
-double Kp = 4, Ki = 7, Kd = 2;
-const int maxPower = 255;
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3D
 
-PID myPID(&tempRead, &Output, &setTemp, Kp, Ki, Kd, REVERSE);
+// -- Globals ---
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+RotaryEncoder encoder(PIN_ENC_A, PIN_ENC_B, RotaryEncoder::LatchMode::FOUR3);
 
-// Voltage sensing variables
-float adc_voltage = 0.0;
-float in_voltage = 0.0;
+// State updated by ISRs
+static volatile int encoderPosition = 0;
+static volatile bool encBtnPressed = false;
+static volatile bool confBtnPressed = false;
+static volatile bool backBtnPressed = false;
 
-float R1 = 30000.0;
-float R2 = 7500.0;
-float ref_voltage = 5.0;
+// 30 Hz render (~33ms)
+static const unsigned long RENDER_INTERVAL_MS = 33;
+static unsigned long lastRenderMs = 0;
 
-int adc_value = 0;
+// --- ISRs ---
+void encoderISR() {
+  encoder.tick();
+  int dir = (int)encoder.getDirection();
+  if (dir != 0) {
+    encoderPosition += -dir;
+  }
+}
+
+void encBtnISR() { encBtnPressed = (digitalRead(PIN_ENC_BTN) == LOW); }
+
+// --- Render ---
+
+void render(int pos, bool encBtn, bool confBtn, bool backBtn) {
+  display.clearDisplay();
+  display.setTextSize(1);
+
+  display.setCursor(0, 0);
+  display.print("Hardware Test");
+  display.drawLine(0, 10, 127, 10, 1);
+
+  display.setCursor(0, 14);
+  display.print("Encoder  ");
+  display.print(pos);
+
+  display.setCursor(0, 26);
+  display.print("Enc Btn  ");
+  display.print(encBtn ? "PRESSED" : "---");
+
+  display.setCursor(0, 38);
+  display.print("Conf Btn ");
+  display.print(confBtn ? "PRESSED" : "---");
+
+  display.setCursor(0, 50);
+  display.print("Back Btn ");
+  display.print(backBtn ? "PRESSED" : "---");
+
+  display.display();
+}
+
+// --- Setup / Loop ---
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(115200);
+  // delay(1000);
 
-  sensors.begin();
+  // Wire.begin();
+  // Wire.setClock(400000);
 
-  pinMode(MOSFET_PIN, OUTPUT);
+  // if (!display.begin(0x3C, true)) {
+  //   Serial.println("SSD1306 failed!");
+  //   while (1)
+  //     delay(1000);
+  // }
+  // display.clearDisplay();
+  // display.setTextColor(SSD1306_WHITE);
+  // display.display();
 
-  setTemp = 20;
+  // pinMode(PIN_ENC_A, INPUT_PULLUP);
+  // pinMode(PIN_ENC_B, INPUT_PULLUP);
+  // pinMode(PIN_ENC_BTN, INPUT_PULLUP);
 
-  myPID.SetOutputLimits(0, maxPower);
-  myPID.SetMode(AUTOMATIC);
+  // attachInterrupt(digitalPinToInterrupt(PIN_ENC_A), encoderISR, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(PIN_ENC_B), encoderISR, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(PIN_ENC_BTN), encBtnISR, CHANGE);
+
+  // render(0, false, false, false);
+  // Serial.println("Ready");
 }
 
 void loop() {
-  // Read temperature
-  sensors.requestTemperatures();
-  tempRead = sensors.getTempCByIndex(0);
+  // unsigned long now = millis();
+  // if (now - lastRenderMs < RENDER_INTERVAL_MS)
+  //   return;
+  // lastRenderMs = now;
 
-  // Read voltage through divider
-  adc_value = analogRead(VOLTAGE_SENSOR_PIN);
-  adc_voltage = (adc_value * ref_voltage) / 1023.0;
-  in_voltage = adc_voltage / (R2 / (R1 + R2));
+  // noInterrupts();
+  // int pos = encoderPosition;
+  // bool encBtn = encBtnPressed;
+  // bool confBtn = confBtnPressed;
+  // bool backBtn = backBtnPressed;
+  // interrupts();
 
-  if (tempRead != DEVICE_DISCONNECTED_C) {
-    myPID.Compute();
-
-    analogWrite(MOSFET_PIN, (int)Output);
-
-    Serial.print("Temp: ");
-    Serial.print(tempRead);
-    Serial.print(" C, Set: ");
-    Serial.print(setTemp);
-    Serial.print(" C, PWM: ");
-    Serial.print((int)Output);
-    Serial.print(", ADC: ");
-    Serial.print(adc_value);
-    Serial.print(", A0 Voltage: ");
-    Serial.print(adc_voltage, 2);
-    Serial.print(" V, Input Voltage: ");
-    Serial.print(in_voltage, 2);
-    Serial.println(" V");
-  } else {
-    analogWrite(MOSFET_PIN, 0);
-    Serial.println("Error: Could not read temperature data");
-  }
-
-  delay(500);
+  // render(pos, encBtn, confBtn, backBtn);
 }
